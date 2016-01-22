a7f756fb597cd918aeecfae92ed15b9bd6b51e6d
haykelbenmassaoud <haykel.ben.masaoud@gmail.com>
2016-01-22 23:58:37 +0100

    hotelmanagementsystem

---
+++ HotelSystem/init_script.sql
@@ -0,0 +1,11 @@
+insert into user (username,password) values ('haykel','haykel');
+
+insert into address(city,street,zipCode)values('TUNIS','LAC','2666');
+insert into location(location,address_id)values('TUNISIA',1);
+insert into hotel(name,location_location) values ('SHERATON','TUNISIA');
+insert into room (number,direction,hotel_name,type) values (8,'WEST','SHERATON','com.hotel.model.StandardRoom');
+insert into room (number,direction,hotel_name,type) values (9,'EAST','SHERATON','com.hotel.model.SpecialRoom');
+insert into room (number,direction,hotel_name,type) values (10,'NORTH','SHERATON','com.hotel.model.ExclusiveRoom');
+insert into room (number,direction,hotel_name,type) values (11,'WEST','SHERATON','com.hotel.model.StandardRoom');
+insert into room (number,direction,hotel_name,type) values (12,'NORTH','SHERATON','com.hotel.model.SpecialRoom');
+insert into room (number,direction,hotel_name,type) values (13,'EAST','SHERATON','com.hotel.model.ExclusiveRoom');
